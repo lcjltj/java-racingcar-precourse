aa2423e21a84a45d748d69934a009e4a123cd7f8
lcjltj <lcjltj@gmail.com>
2022-04-23 21:29:08 +0900

    test : racer 테스트 케이스 추가
    - Location 이동 거리 관련 테스트 케이스 추가
    - 레이싱 이름 테스트 케이스 추가

--- src/test/java/racingcar/domain/wrapper/LocationTest.java
+++ src/test/java/racingcar/domain/wrapper/LocationTest.java
@@ -1,25 +1,26 @@
-package racingcar.domain;
+package racingcar.domain.wrapper;
 
 import org.junit.jupiter.api.Test;
+import racingcar.domain.wrapper.Location;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocationTest {
     @Test
     void 이동() {
-        String move = "8";
         Location location = new Location();
-        location.move(move);
+        location.move(8);
 
         assertEquals(1, location.getLocation());
+        assertEquals("-", location.getDashDistance());
     }
 
     @Test
     void 멈춤() {
-        String move = "3";
         Location location = new Location();
-        location.move(move);
+        location.move(3);
 
         assertEquals(0, location.getLocation());
+        assertEquals("", location.getDashDistance());
     }
 }
